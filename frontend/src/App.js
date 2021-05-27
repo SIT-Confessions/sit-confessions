0434f7407624bb0ea729c994805b383b977a49f6
Gabriel Kok <gabrielkok4227@gmail.com>
2021-05-27 21:32:02 +0800

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
@@ -390,6 +391,7 @@ function App() {
 
 
 <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+  <Navbar></Navbar>
       <div className="max-w-md w-full space-y-8">
         <div>
           <img
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div class="bg-white dark:bg-gray-800">
+        <h1 class="text-gray-900 dark:text-white">Dark mode is here!</h1>
+        <p class="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
