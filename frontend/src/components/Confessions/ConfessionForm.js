b87f8c2bfbf954031e4709b5abd217003abc35f4
Gabriel Kok <gabrielkok4227@gmail.com>
2021-05-30 16:53:32 +0800

    Modified Confession Form

--- frontend/src/components/Confessions/ConfessionForm.js
+++ frontend/src/components/Confessions/ConfessionForm.js
@@ -7,126 +7,70 @@ const ConfessionForm = () => {
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Profile
-              </h3>
-              <p className="mt-1 text-sm text-gray-600">
-                This information will be displayed publicly so be careful what
-                you share.
-              </p>
+              <article className="prose">
+                <h2>Compose New Confession</h2>
+                <p>
+                  Have an interesting story to share? Write it all down here!
+                  Your post will be anonymous.
+                </p>
+                <h4>Some important notes:</h4>
+                <ul>
+                  <li>
+                    We are not affiliated with the Singapore Institute of
+                    Technology in any way.
+                  </li>
+                  <li>
+                    Please be socially responsible and do not post any sensitive
+                    or hurtful content. No racist, vulgar, sexual or other
+                    inappropriate content is allowed.
+                  </li>
+                  <li>
+                    All submissions will be moderated before they are publicly
+                    viewable.
+                  </li>
+                </ul>
+              </article>
             </div>
           </div>
           <div className="mt-5 md:mt-0 md:col-span-2">
             <form action="#" method="POST">
               <div className="shadow sm:rounded-md sm:overflow-hidden">
                 <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <label
-                        htmlFor="company_website"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Website
-                      </label>
-                      <div className="mt-1 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                          http://
-                        </span>
-                        <input
-                          type="text"
-                          name="company_website"
-                          id="company_website"
-                          className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                          placeholder="www.example.com"
-                        />
-                      </div>
-                    </div>
-                  </div>
-
                   <div>
                     <label
-                      htmlFor="about"
+                      htmlFor="Confession"
                       className="block text-sm font-medium text-gray-700"
                     >
-                      About
+                      Write Your Confession
                     </label>
                     <div className="mt-1">
                       <textarea
-                        id="about"
-                        name="about"
-                        rows={3}
+                        id="confession"
+                        name="confession"
+                        rows={10}
                         className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                        placeholder="you@example.com"
+                        placeholder="Your wonderful story goes in here."
                         defaultValue={""}
                       />
                     </div>
-                    <p className="mt-2 text-sm text-gray-500">
-                      Brief description for your profile. URLs are hyperlinked.
-                    </p>
+                    <p className="mt-2 text-sm text-gray-500">Tell us more about the story (:</p>
                   </div>
 
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                      Photo
-                    </label>
-                    <div className="mt-1 flex items-center">
-                      <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                        <svg
-                          className="h-full w-full text-gray-300"
-                          fill="currentColor"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                        </svg>
-                      </span>
-                      <button
-                        type="button"
-                        className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        Change
-                      </button>
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="agree"
+                        name="agree"
+                        type="checkbox"
+                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                      />
                     </div>
-                  </div>
-
-                  <div>
-                    <label className="block text-sm font-medium text-gray-700">
-                      Cover photo
-                    </label>
-                    <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                      <div className="space-y-1 text-center">
-                        <svg
-                          className="mx-auto h-12 w-12 text-gray-400"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 48 48"
-                          aria-hidden="true"
-                        >
-                          <path
-                            d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                            strokeWidth={2}
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                        <div className="flex text-sm text-gray-600">
-                          <label
-                            htmlFor="file-upload"
-                            className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                          >
-                            <span>Upload a file</span>
-                            <input
-                              id="file-upload"
-                              name="file-upload"
-                              type="file"
-                              className="sr-only"
-                            />
-                          </label>
-                          <p className="pl-1">or drag and drop</p>
-                        </div>
-                        <p className="text-xs text-gray-500">
-                          PNG, JPG, GIF up to 10MB
-                        </p>
-                      </div>
+                    <div className="ml-3 text-sm">
+                      <p className="text-gray-500">
+                        I have read the important notes and agree to post
+                        content that will not be offensive, hurtful and
+                        insensitive.
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -135,7 +79,7 @@ const ConfessionForm = () => {
                     type="submit"
                     className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
-                    Save
+                    Submit
                   </button>
                 </div>
               </div>
