1dcf6f404ce079ca267298d75f5331d588287850
Gabriel Kok <gabrielkok4227@gmail.com>
2021-05-30 20:32:57 +0800

    UI Improvements

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -56,7 +56,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <Disclosure as="nav" className="bg-gray-800">
+      <Disclosure as="nav" className="bg-gray-800 dark:bg-gray-800">
         {({ open }) => (
           <>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,95 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray-lightest": "#303030",
+        "dark-gray-lighter": "#2D2D2D",
+        "dark-gray-light": "#282828",
+        "dark-gray": "#232323",
+        "dark-gray-dark": "#202020",
+        "dark-gray-darker": "#1C1C1C",
+        "dark-gray-darkest": "#191919",
+      },
+      typography: (theme) => ({
+        default: {
+          css: {
+            color: theme("colors.gray.900"),
+            a: {
+              color: theme("colors.blue.500"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+              },
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.300'),
+              },
+              a: {
+                color: theme('colors.white'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.600'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.200'),
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.600'),
+              },
+              h1: {
+                color: theme('colors.gray.100'),
+              },
+              h2: {
+                color: theme('colors.gray.100'),
+              },
+              h3: {
+                color: theme('colors.gray.100'),
+              },
+              h4: {
+                color: theme('colors.gray.100'),
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.400'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600'),
+              },
+          },
+        },
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      typography: ['dark'],
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
