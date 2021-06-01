ada66d314298639618d62cd6485c25e6b0f0938e
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-01 13:08:52 +0800

    Sampling cards for confessions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,12 +46,18 @@ function App() {
 const Home = () => (
   <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
     <div class="shadow-lg rounded-2xl p-4 bg-white relative overflow-hidden">
-      <div class="w-4/6">
+      <div className="sm:min-w-0 max-w-xl">
         <p class="text-gray-800 text-lg font-medium mb-2">#3195</p>
-        <p class="text-gray-400 text-xs">
-          Detail is not an obsession, it is the very essence of perfection.
+        <p class="text-gray-600 text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
         </p>
-        <p class="text-indigo-500 text-xl font-medium">$399</p>
+        <div className="flex mt-4 justify-between">
+          <a href="#" className="text-sm text-indigo-600 hover:text-indigo-500">
+            View on Facebook
+          </a>
+          <div className="text-gray-400 text-sm">3 mins ago</div> 
+        </div>
+        
       </div>
     </div>
     <div className="max-w-md w-full space-y-8">
