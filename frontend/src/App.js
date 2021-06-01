01d71dbd4d754b2f00bfec9b2ccaf27d02308631
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-01 14:57:35 +0800

    Moved home page to separate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { AdjustmentsIcon } from "@heroicons/react/outline";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ConfessionForm from "./components/Confessions/ConfessionForm";
 import NotFound from "./components/NotFound";
+import Home from "./components/Home";
 import { Helmet } from "react-helmet";
 
 function App() {
@@ -42,117 +43,5 @@ function App() {
     </Router>
   );
 }
-
-const Home = () => (
-  <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-    <div class="shadow-lg rounded-2xl p-4 bg-white relative overflow-hidden">
-      <div className="sm:min-w-0 max-w-xl">
-        <p class="text-gray-800 text-lg font-medium mb-2">#3195</p>
-        <p class="text-gray-600 text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-        </p>
-        <div className="flex mt-4 justify-between">
-          <a href="#" className="text-sm text-indigo-600 hover:text-indigo-500">
-            View on Facebook
-          </a>
-          <div className="text-gray-400 text-sm">3 mins ago</div> 
-        </div>
-        
-      </div>
-    </div>
-    <div className="max-w-md w-full space-y-8">
-      <div>
-        <img
-          className="mx-auto h-12 w-auto"
-          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-          alt="Workflow"
-        />
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Sign in to your account
-        </h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Or{" "}
-          <a
-            href="#"
-            className="font-medium text-indigo-600 hover:text-indigo-500"
-          >
-            start your 14-day free trial
-          </a>
-        </p>
-      </div>
-      <form className="mt-8 space-y-6" action="#" method="POST">
-        <input type="hidden" name="remember" defaultValue="true" />
-        <div className="rounded-md shadow-sm -space-y-px">
-          <div>
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="sr-only">
-              Password
-            </label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <input
-              id="remember_me"
-              name="remember_me"
-              type="checkbox"
-              className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-            />
-            <label
-              htmlFor="remember_me"
-              className="ml-2 block text-sm text-gray-900"
-            >
-              Remember me
-            </label>
-          </div>
-
-          <div className="text-sm">
-            <a
-              href="#"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              Forgot your password?
-            </a>
-          </div>
-        </div>
-
-        <div>
-          <button
-            type="submit"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-              <AdjustmentsIcon className="h-5 w-5 text-white-500" />
-            </span>
-            Sign in
-          </button>
-        </div>
-      </form>
-    </div>
-  </div>
-);
 
 export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div class="flow-root shadow-lg rounded-2xl p-4 bg-white relative overflow-hidden">
+          <div className="sm:min-w-0 max-w-xl">
+            <p class="text-gray-800 text-lg font-medium mb-2">#3195</p>
+            <p class="text-gray-600 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            </p>
+            <div className="flex mt-4 justify-between">
+              <a href="#" className="text-sm text-indigo-600 hover:text-indigo-500">
+                View on Facebook
+              </a>
+              <div className="text-gray-400 text-sm">14 May 2021, 7:44 PM</div> 
+            </div>
+            
+          </div>
+        </div>
+        <div class="flow-root shadow-lg rounded-2xl p-4 bg-white relative overflow-hidden">
+          <div className="sm:min-w-0 max-w-xl">
+            <p class="text-gray-800 text-lg font-medium mb-2">#3195</p>
+            <p class="text-gray-600 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            </p>
+            <div className="flex mt-4 justify-between">
+              <a href="#" className="text-sm text-indigo-600 hover:text-indigo-500">
+                View on Facebook
+              </a>
+              <div className="text-gray-400 text-sm">14 May 2021, 7:44 PM</div> 
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Home
