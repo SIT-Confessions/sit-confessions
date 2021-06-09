566494539f57516f685141da4b706645e530b39a
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-09 22:25:00 +0800

    UI Tweaks and Improvements

--- frontend/src/components/Admin/ConfessionDetailsModal.js
+++ frontend/src/components/Admin/ConfessionDetailsModal.js
@@ -153,7 +153,7 @@ const ConfessionDetailsModal = (props) => {
                 ) : null}
                 <button
                   type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 dark:border-gray-600 dark:text-gray-200 dark:bg-dark-gray dark:hover:bg-dark-gray-lighter focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={() => setOpen(false)}
                   ref={cancelButtonRef}
                 >
