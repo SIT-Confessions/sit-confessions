0df03364b32b007411185e56d95b20c62e7d541c
HanYi <nghanyi1997@gmail.com>
2021-06-11 17:00:28 +0800

    slow down request response after 50 requests has been sent from the same ip

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cron from "node-cron";
 import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { postToFB } from "./controllers/confessions.js";
@@ -20,10 +21,17 @@ const app = express();
 connectDB();
 
 // Limit to 100 request per 15mins per IP address
-const limiter = rateLimit({
+const rateLimiter = rateLimit({
   max: 100,
   windowMs: 15 * 60 * 1000,
   message: "Too many request from this IP",
+});
+
+// Slow down speed of request after 50 requests in 10mins
+const speedLimiter = slowDown({
+  windowMs: 10 * 60 * 1000,
+  delayAfter: 50,
+  delayMs: 500,
 });
 
 // Log api requests to log file
@@ -38,7 +46,8 @@ const accessLogStream = fs.createWriteStream(
 
 app.set("trust proxy", 1);
 
-app.use(limiter);
+app.use(rateLimiter);
+app.use(speedLimiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
