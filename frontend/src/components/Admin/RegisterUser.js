198341b471e727b0d10448432b75b98214daa465
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-21 13:03:46 +0800

    Register User Form now works

--- frontend/src/components/Admin/RegisterUser.js
+++ frontend/src/components/Admin/RegisterUser.js
@@ -1,50 +1,85 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addNotification } from "../../actions";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `http://localhost:5000/api/`,
+});
 
 const RegisterUser = () => {
+  const dispatch = useDispatch();
   const [registerFormData, setRegisterFormData] = useState({
     name: "",
     email: "",
     role: "",
     password: "",
+    password2: "",
   });
 
   const nameChangeHandler = (event) => {
-    setRegisterFormData = (prevState) => {
+    setRegisterFormData((prevState) => {
       return { ...prevState, name: event.target.value };
-    };
+    });
   };
 
-  const emailChangeHandler = () => {
-    setRegisterFormData = (prevState) => {
+  const emailChangeHandler = (event) => {
+    setRegisterFormData((prevState) => {
       return { ...prevState, email: event.target.value };
-    };
+    });
   };
 
-  const roleChangeHandler = () => {
-    setRegisterFormData = (prevState) => {
+  const roleChangeHandler = (event) => {
+    setRegisterFormData((prevState) => {
       return { ...prevState, role: event.target.value };
-    };
+    });
   };
 
-  const passwordChangeHandler = () => {
-    setRegisterFormData = (prevState) => {
+  const passwordChangeHandler = (event) => {
+    setRegisterFormData((prevState) => {
       return { ...prevState, password: event.target.value };
-    };
+    });
   };
 
-  const handleRegister = (e) => {
+  const confirmPasswordChangeHandler = (event) => {
+    setRegisterFormData((prevState) => {
+      return { ...prevState, password2: event.target.value };
+    });
+  };
+
+  const handleRegister = async (e) => {
     e.preventDefault();
-    alert("To Do");
+    let data = registerFormData;
+    let res = await api.post("/users", data);
+    clearInputs();
+    ShowNotification({
+      id: uuidv4(),
+      title: "Successfully Submitted!",
+      message: data.name + " has been successfully registered!",
+      type: "success",
+    });
+  };
+
+  const clearInputs = () => {
+    setRegisterFormData(() => {
+      return {
+        name: "",
+        email: "",
+        role: "",
+        password: "",
+        password2: "",
+      };
+    });
+  };
+
+  const ShowNotification = (data) => {
+    dispatch(addNotification(data));
   };
 
   return (
     <div className="flex justify-center px-2 md:px-0">
       <div className="transition-colors duration-500 bg-gray-50 dark:bg-dark-gray-light overflow-auto rounded-xl shadow-lg md:w-1/2">
-        {/* <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg leading-6 font-medium text-gray-800">
-            Profile Information
-          </h3>
-        </div> */}
         <div className="border-gray-200">
           <form onSubmit={handleRegister}>
             <div className="shadow sm:rounded-md sm:overflow-hidden">
@@ -72,6 +107,8 @@ const RegisterUser = () => {
                       id="name"
                       className="transition-colors duration-500 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300 dark:border-gray-700 dark:bg-dark-gray-darkest dark:text-gray-100"
                       placeholder="Karen"
+                      value={registerFormData.name}
+                      onChange={nameChangeHandler}
                     />
                   </div>
                 </div>
@@ -90,6 +127,8 @@ const RegisterUser = () => {
                       id="email"
                       className="transition-colors duration-500 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-md sm:text-sm border-gray-300 dark:border-gray-700 dark:bg-dark-gray-darkest dark:text-gray-100"
                       placeholder="karen@wheresyourbadge.com"
+                      value={registerFormData.email}
+                      onChange={emailChangeHandler}
                     />
                   </div>
                 </div>
@@ -107,6 +146,27 @@ const RegisterUser = () => {
                       name="password"
                       id="password"
                       className="transition-colors duration-500 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-md sm:text-sm border-gray-300 dark:border-gray-700 dark:bg-dark-gray-darkest dark:text-gray-100"
+                      value={registerFormData.password}
+                      onChange={passwordChangeHandler}
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full lg:w-3/4">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium transition-colors duration-500 text-gray-700 dark:text-gray-300"
+                  >
+                    Confirm Password
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="confirmPassword"
+                      id="confirmPassword"
+                      className="transition-colors duration-500 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-md sm:text-sm border-gray-300 dark:border-gray-700 dark:bg-dark-gray-darkest dark:text-gray-100"
+                      value={registerFormData.password2}
+                      onChange={confirmPasswordChangeHandler}
                     />
                   </div>
                 </div>
@@ -123,6 +183,8 @@ const RegisterUser = () => {
                     name="role"
                     autoComplete="role"
                     className="transition-colors duration-500 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:border-gray-700 dark:bg-dark-gray-darkest dark:text-gray-100"
+                    value={registerFormData.role}
+                    onChange={roleChangeHandler}
                   >
                     <option value="" disabled selected>
                       Select Role
@@ -131,48 +193,6 @@ const RegisterUser = () => {
                     <option value="admin">Admin</option>
                   </select>
                 </div>
-
-                {/* <div>
-                  <label className="block text-sm font-medium text-gray-700">
-                    Cover photo
-                  </label>
-                  <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                    <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                        >
-                          <span>Upload a file</span>
-                          <input
-                            id="file-upload"
-                            name="file-upload"
-                            type="file"
-                            className="sr-only"
-                          />
-                        </label>
-                        <p className="pl-1">or drag and drop</p>
-                      </div>
-                      <p className="text-xs text-gray-500">
-                        PNG, JPG, GIF up to 10MB
-                      </p>
-                    </div>
-                  </div>
-                </div> */}
               </div>
               <div className="px-4 py-3 transition-colors duration-500 bg-gray-50 dark:bg-dark-gray-light text-center sm:px-6">
                 <button
