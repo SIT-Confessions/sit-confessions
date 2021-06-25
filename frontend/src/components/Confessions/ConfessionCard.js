dbc242a178a0df35c69cb8ac68ca46d86a0abe32
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-25 21:55:59 +0800

    General improvements

--- frontend/src/components/Confessions/ConfessionCard.js
+++ frontend/src/components/Confessions/ConfessionCard.js
@@ -13,13 +13,13 @@ const ConfessionCard = (props) => {
     <div className="flex-shrink-0 items-center justify-center transition-colors duration-500 bg-gray-50 max-w-full sm:w-full min-w-min py-4 dark:bg-dark-gray">
       <div class="flow-root shadow-lg rounded-2xl p-4 transition-colors duration-500 bg-white relative overflow-hidden dark:bg-dark-gray-lighter">
         <div className="sm:min-w-0">
-          <p class="transition-colors duration-500 text-gray-800 dark:text-gray-50 text-lg font-medium mb-2">
+          <p class="transition-colors duration-500 text-gray-800 dark:text-gray-50 text-xl font-medium mb-4">
             #{itemData._id}
           </p>
           <p class="transition-colors duration-500 text-gray-600 dark:text-gray-200 text-md whitespace-pre-line font-medium">
             {he.decode(itemData.text)}
           </p>
-          <div className="flex mt-4 justify-between">
+          <div className="flex mt-6 justify-between">
             <a
               href={itemData.fbURL}
               target="_blank"
--- frontend/src/components/Confessions/ConfessionsFeed.js
+++ frontend/src/components/Confessions/ConfessionsFeed.js
@@ -1,8 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { useState, useRef, useEffect, Fragment } from 'react';
 import ConfessionCard from "./ConfessionCard";
 
 const ConfessionsFeed = (props) => {
     let confessionsData = props.confessions.posts;
+    const [page, setPage] = useState(1);
+    const loader = useRef(null);
+
+    useEffect(() => {
+        var options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 1.0
+         };
+
+         const observer = new IntersectionObserver(handleObserver, options);
+         if (loader.current) {
+            observer.observe(loader.current)
+         }
+    }, [])
+
+    useEffect(() => {
+        // Append more confessions to be shown
+        alert("show more confessions")
+    }, [page])
+
+    const handleObserver = (entities) => {
+        const target = entities[0];
+        if (target.isIntersecting) {   
+            setPage((page) => page + 1)
+        }
+    }
+
     return (
       <>
         {confessionsData?.map((item, itemIDx) => (
@@ -10,6 +38,7 @@ const ConfessionsFeed = (props) => {
             <ConfessionCard data={item} />
           </Fragment>
         ))}
+        <div ref={loader}></div>
       </>
     );
 }
