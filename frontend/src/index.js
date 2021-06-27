d078ef6240727c2781a897c7fc0b58c00e356e41
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-28 01:32:57 +0800

    SITC-91 Fixed premature logout when refreshing dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 // const store = createStore(
 //   baseReducer,
@@ -12,8 +13,10 @@ import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
