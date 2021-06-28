0a01669d2d29aaf522996862c16b32890e3c7c7f
Gabriel Kok <gabrielkok4227@gmail.com>
2021-06-28 15:52:19 +0800

    Misc UI Improvements

--- frontend/src/components/Admin/Users.js
+++ frontend/src/components/Admin/Users.js
@@ -26,7 +26,7 @@ const Users = () => {
         <Link to="/register-user" exact>
         <button
           type="button"
-          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
           
         >
           Add New User
