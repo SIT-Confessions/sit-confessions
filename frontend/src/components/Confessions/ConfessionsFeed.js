cf89a268ea7b402ea49c2c5a64fb0bbf4e1330c3
Gabriel <gabrielkok4227@gmail.com>
2021-07-04 15:10:23 +0800

    Added Loading UI for ConfessionsFeed

--- frontend/src/components/Confessions/ConfessionsFeed.js
+++ frontend/src/components/Confessions/ConfessionsFeed.js
@@ -63,8 +63,8 @@ const ConfessionsFeed = () => {
     // Append more confessions to be shown
     // console.log("loader triggered", page);
     // console.log("443",allLoaded)
-    if(!allLoaded)
-        getData();
+    if (!allLoaded)
+      getData();
   }, [page]);
 
   const handleObserver = (entities) => {
@@ -72,7 +72,7 @@ const ConfessionsFeed = () => {
     // console.log("isIntersecting", target.isIntersecting);
     // console.log("allLoaded", allLoaded);
     if (target.isIntersecting) {
-    //   console.log("442",allLoaded);
+      //   console.log("442",allLoaded);
       setPage((page) => page + 1);
     }
   };
@@ -86,7 +86,61 @@ const ConfessionsFeed = () => {
           </Fragment>
         ))}
       </div>
-      <div ref={loader}></div>
+      {!allLoaded && !isPopulated ? (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <div className="max-w-max w-full text-center pb-4 flex">
+            <svg
+              class="animate-spin -ml-1 mr-3 h-8 w-8 text-gray-800 dark:text-gray-100"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+              ></circle>
+              <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span className="font-bold text-xl text-gray-500 dark:text-gray-400">
+              Loading Confessions...
+            </span>
+          </div>
+        </div>
+      ) : (
+        // <div ref={loader}></div>
+        !allLoaded ? 
+        (<div ref={loader} className="justify-center items-center flex mt-12">
+          <svg
+            class="animate-spin -ml-1 mr-3 h-8 w-8 text-gray-400 dark:text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+        ) : null
+      )}
     </>
   );
 };
